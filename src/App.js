135909bacc0b710a71b6cb25efbe3c2437ae2822
NandiphaNdlovu <biameleng@icloud.com>
2023-08-18 14:35:37 +0200

    added interactive particles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './assets/img/logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Contact } from './components/Contact';
+import Particles from './components/particles';
+
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Particles/>
       <Contact/>
     </div>
   );
