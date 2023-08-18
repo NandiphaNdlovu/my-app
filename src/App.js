79971b1998833f24a1b7143e33cfa94700398af7
NandiphaNdlovu <biameleng@icloud.com>
2023-08-18 17:01:52 +0200

    banner and introduction

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import logo from './assets/img/logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+import { Banner } from './components/Banner';
 import Particles from './components/Particles';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <Particles/>
+      <NavBar />
+      <Banner/>
       <Contact/>
+      {/*<Footer/>*/}
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Software Developer", "Web Developer"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Hello:) I'm Nandipha Ndlovu</span>
+                <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer", "Web Developer"]'><span className="wrap">{text}</span></span></h1>
+                  <p> I have a passion for anything involving Computer Science. I've been able to hone my skills in Software Engineering and Project Management, and I'm always looking for new ways to challenge myself and keep growing.
+                  When I'm not working, you can usually find me outside exploring my environment or inside behind the monitor playing games with friends. I believe in the importance of maintaining a healthy work-life balance, and I always make sure to take time for the things that matter most to me.
+                  Overall, I'm a positive, can-do kind of person who loves taking on new challenges and pushing myself to be the best I can be. Whether I'm working on a project for a client or just trying to improve myself in some way, I always give it my all and aim for excellence.
+                  </p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            {/*<TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>*/}
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
