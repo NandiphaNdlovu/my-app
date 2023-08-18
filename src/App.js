296c70e1b386bfb61e8b51ae2288884586d535ae
NandiphaNdlovu <biameleng@icloud.com>
2023-08-18 13:56:13 +0200

    nav bar and its styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import logo from './assets/img/logo.svg';
 import './App.css';
-
+import { NavBar } from './components/NavBar';
+import { Contact } from './components/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
+      <Contact/>
     </div>
   );
 }
